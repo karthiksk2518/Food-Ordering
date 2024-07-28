ff5f23ff14cca5b5c3a7fd7de230321921bef8e1
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-07-28 14:55:55 +0530

    Footer Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,20 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="w-[80%] m-auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder />} />
-      </Routes>
+    <div>
+      <div className="w-[80%] m-auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer/>
     </div>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import { assets } from "../assets/assets"
+
+const Footer = () => {
+    return (
+        <div className="text-[#d9d9d9] bg-[#323232] flex flex-col items-center gap-5 py-5 px-[8vw] pt-8 mt-[30px]" id="footer">
+            <div className="w-full grid grid-cols-4 gap-20">
+                <div className="col-span-2 flex flex-col items-start gap-5">
+                    <img src={assets.logo} alt="" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure tempora repudiandae labore voluptatem. Culpa assumenda sint nostrum vitae! Quo facere libero laborum similique at temporibus veritatis fugiat earum officiis sapiente!</p>
+                    <div className="flex ">
+                        <img className="w-10 mr-[15px] cursor-pointer" src={assets.facebook_icon} alt="" />
+                        <img className="w-10 mr-[15px] cursor-pointer" src={assets.twitter_icon} alt="" />
+                        <img className="w-10 mr-[15px] cursor-pointer" src={assets.linkedin_icon} alt="" />
+                    </div>
+                </div>
+                <div className="col-span-1 flex flex-col items-start gap-5">
+                    <h2 className="text-white">COMPANY</h2>
+                    <ul>
+                        <li className="mb-2.5 cursor-pointer">Home</li>
+                        <li className="mb-2.5 cursor-pointer">About Us</li>
+                        <li className="mb-2.5 cursor-pointer">Delivery</li>
+                        <li className="mb-2.5 cursor-pointer">Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className="col-span-1 flex flex-col items-start gap-5">
+                    <h2 className="text-white">GET IN TOUCH</h2>
+                    <ul>
+                        <li className="mb-2.5 cursor-pointer">+91-7566236975</li>
+                        <li className="mb-2.5 cursor-pointer">karthikpatidar2518@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr className="w-[100%] h-0.5 my-5 mx-0 bg-gray-500 border-none"/>
+            <p className="footer-copyright">
+                &copy; 2024 Karthik Patidar - All Right Reserved.
+            </p>
+        </div>
+    )
+}
+
+export default Footer
