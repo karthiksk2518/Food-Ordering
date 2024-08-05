2f8d997ee8e7d1eb942c18e9bcd3f51ba6deebaf
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-08-05 18:12:12 +0530

    somw properties added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '750px',
+      'md': '900px',
+      'lg': '1050px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontSize: {
         'responsive': 'max(1.4vw, 16px)',
