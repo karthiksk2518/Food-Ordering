d1440ea76116500618edaf046f81f2f2e26768e2
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-08-05 18:23:51 +0530

    Navbar complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
     return (
         <div className="pt-5 w-[100%] flex justify-between items-center navbar">
-            <img src={assets.logo} alt="" className="w-[120px] md:w-[150px] navbar-logo" />
+            <img src={assets.logo} alt="" className="w-[120px] md:w-[140px] lg:w-[150px] navbar-logo" />
             <ul className="hidden md:flex list-none md:gap-3 lg:gap-5 text-[#49557e] sm:text-[16px] md:text-[18px]">
                 <li onClick={() => setMenu("home")} className={`${menu === 'home' ? 'pb-1 border-b-2 border-[#49557e]' : ''} cursor-pointer`}>
                     Home
@@ -26,11 +26,11 @@ const Navbar = () => {
                 </li>
             </ul>
             <div className="flex items-center gap-4 sm:gap-4 md:gap-7">
-                <IoSearch className='text-2xl md:text-3xl' />
-                <MdShoppingCart className='text-2xl md:text-3xl' />
+                <IoSearch className='text-2xl md:text-3xl lg:text-4xl' />
+                <MdShoppingCart className='text-2xl md:text-3xl lg:text-4xl' />
                 <button
                     className='bg-transparent text-[14px] md:text-[16px] text-[#49557e] border-2 border-solid border-orange-800 
-                    p-[6px_14px] md:p-[10px_30px] rounded-[40px] cursor-pointer hover:bg-[#fff4f2] transition-all duration-300'
+                    p-[7px_20px] md:p-[8px_25px] lg:p-[10px_30px] rounded-[40px] cursor-pointer hover:bg-[#fff4f2] transition-all duration-300'
                 >
                     Sign In
                 </button>
