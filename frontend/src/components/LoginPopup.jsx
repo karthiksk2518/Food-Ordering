80a95ada7297cdc91e47e0c06ac46ea33c9896a0
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-08-06 15:29:17 +0530

    animation added

--- frontend/src/components/LoginPopup.jsx
+++ frontend/src/components/LoginPopup.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { ImCancelCircle } from "react-icons/im";
+import './LoginPopup.css'
 
 const LoginPopup = ({ setShowLogin }) => {
 
@@ -16,7 +17,7 @@ const LoginPopup = ({ setShowLogin }) => {
     }, []);
 
     return (
-        <div className="flex flex-col justify-center items-center w-[100%] h-[100%] bg-[#00000090] fixed z-50">
+        <div className="flex flex-col justify-center items-center w-[100%] h-[100%] bg-[#00000090] fixed z-50 fadeIn">
             <form className="login-popup-container w-[90%] sm:w-full flex flex-col gap-5 relative z-10 bg-white rounded-[10px] shadow-xl p-[25px_30px]">
                 <div className="login-popup-title flex justify-between items-center">
                     <h2 className="text-2xl font-semibold">{currState}</h2>
