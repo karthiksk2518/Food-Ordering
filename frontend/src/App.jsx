4c55f6a449bb09538f75cb7b330ee60d841cefc0
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-08-06 14:56:48 +0530

    changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,18 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
 import Footer from "./components/Footer";
+import { useState } from "react";
+import LoginPopup from "./components/LoginPopup";
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+  
   return (
     <div>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin}/> : <></>}
       <div className="w-[80%] m-auto">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
