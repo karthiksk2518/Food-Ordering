ba7b31add055d356a3661007c629504bb01deb8d
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-08-07 11:47:19 +0530

    Hot toast

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './Context/StoreContext.jsx';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StoreContextProvider>
       <App />
+      <Toaster />
     </StoreContextProvider>
   </BrowserRouter>
 
