32b1ca75f0b2dce7384cf76fa17c2ad962daab2e
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-09-10 12:03:34 +0530

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-// exports.connectDB = async () => {
-//     await mongoose.connect(process.env.MONGODB_URL)
-//     .then(() => console.log("MongoDB Connected..."))
-//     .catch(err => console.log(err));
-// }
  
 exports.connectDB = async () => {
   try {
