e1b820eba4549b3e41e09fce0402d969dba8b27f
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-09-10 11:37:13 +0530

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-exports.connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URL)
-    .then(() => console.log("MongoDB Connected..."))
-    .catch(err => console.log(err));
-}
+// exports.connectDB = async () => {
+//     await mongoose.connect(process.env.MONGODB_URL)
+//     .then(() => console.log("MongoDB Connected..."))
+//     .catch(err => console.log(err));
+// }
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
