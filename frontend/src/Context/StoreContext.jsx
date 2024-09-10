0a0768556380f3da0b9c8a7b49ce4b4d22edb45a
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-09-10 12:01:56 +0530

    commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://food-ordering-backend-gbvs.onrender.com";
     const [token, setToken] = useState("");
     const [food_list, setfood_list] = useState([]);
 
