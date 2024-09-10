8619e6b4fa6048170d02d3eded7f404534fd93a2
Kundan Patidar <karthikpatidar2518@gmail.com>
2024-09-10 12:49:55 +0530

    commit

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,6 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 exports.placeOrder = async (req, res) => {
 
     const frontend_url = "https://food-ordering-kp.vercel.app/";
+    
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
