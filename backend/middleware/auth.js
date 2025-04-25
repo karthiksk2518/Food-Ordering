ee21660d857e351ed2c4d5469fdfd2a80adf23c7
Kundan Patidar <karthikpatidar2518@gmail.com>
2025-04-25 17:14:27 +0530

    Issue Fixed

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,45 +1,23 @@
-// const jwt = require("jsonwebtoken");
-
-// exports.authMiddleware = async (req,res,next) => {
-//     const {token} = req.headers;
-//     if(!token) {
-//         return res.json({
-//             success: false,
-//             message: "Not Authorized, Login Again"
-//         });
-//     }
-//     try {
-//         const token_decode = jwt.verify(token,process.env.JWT_SECRET);
-//         req.body.userId = token_decode.id;
-//         next();
-//     } catch (error) {
-//         console.log(error);
-//         res.json({
-//             success: false,
-//             message: "Error Occured"
-//         });
-//     }
-// }
-
 const jwt = require("jsonwebtoken");
 
-exports.authMiddleware = async (req, res, next) => {
-    const { token } = req.headers;
-    if (!token) {
+exports.authMiddleware = async (req,res,next) => {
+    const {token} = req.headers;
+    if(!token) {
         return res.json({
             success: false,
             message: "Not Authorized, Login Again"
         });
     }
     try {
-        const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = { id: token_decode.id }; // Save user info in req.user
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
+        req.body.userId = token_decode.id;
         next();
     } catch (error) {
         console.log(error);
         res.json({
             success: false,
-            message: "Error Occurred"
+            message: "Error Occured"
         });
     }
-};
+}
+
