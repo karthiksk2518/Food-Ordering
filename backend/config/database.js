bee29ac8c49a6fe23c093468c94eba26b1ed5f2d
Kundan Patidar <karthikpatidar2518@gmail.com>
2025-07-10 11:47:25 +0530

    error fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,10 +3,7 @@ require("dotenv").config();
  
 exports.connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
